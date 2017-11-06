d14b47d74d2b0479444634ab253dbff3d0a61d1b
adsznzhang <adszn@hotmail.com>
2017-11-06 22:35:39 +0800

    second day

--- typing/underscore.js
+++ typing/underscore.js
@@ -41,6 +41,50 @@
 
     _.VERSION = '1.8.3';
 
+    var optimizeCb = function(func, context, argCount) {
+        if(context === void 0) return func;
+        switch (argCount == null ? 3 : argCount) {
+        case 1:
+            return function (value) {
+                return func.call(context, value);
+            };
+        case 3:
+            return function (value, index, collecttion) {
+                return func.call(context, value, index, collecttion);
+            };
+        case 4:
+            return function(accumulator, value, index, collection) {
+                return func.call(context,accumulator,value,index,collection);
+            };
+        }
+        return function() {
+            return func.apply(context,arguments);
+        };
+    };
+
+    var bultinIteratee;
+
+    var cb = function (value, context, argCount) {
+        if(_.iteratee !== bultinIteratee) return _.iteratee (value,context);
+
+        if(value == null) return _identity;
+
+        if(_.isFunction(value)) return optimizeCb(value,context, argCount);
+
+        if(_.isObject(value)) return _.matcher(value);
+
+        return _.property(value);
+    };
+
+
+    _.iteratee = bultinIteratee = function (value,context) {
+        return cb(value, context, Infinity);
+    };
+
+
+    var restArgs = function (func, )
+
+
 
 
 
