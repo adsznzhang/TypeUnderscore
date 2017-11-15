589c8316683cbefb7cc9fad4f9b7fbd438a24a50
adsznzhang <adszn@hotmail.com>
2017-11-15 22:53:25 +0800

    Add few functions

--- typing/underscore.js
+++ typing/underscore.js
@@ -82,8 +82,50 @@
     };
 
 
-    var restArgs = function (func, )
+    var restArgs = function (func, startIndex){
+        startIndex = startIndex == null ? func.length - 1: +startIndex;
+        return function() {
+            var length = Math.max(arguments.length - startIndex, 0);
+            var rest = Array(length);
+            for(var index = 0; index < length; index++){
+                rest[index] =arguments[index + startIndex];
+            }
 
+            switch (startIndex) {
+            case 0:
+                return func.call(this, rest);
+            case 1:
+                return func.call(this, arguments[0], rest);
+            case 2:
+                return func.call(this, arguments[0],arguments[1], rest);
+            }
+
+            var args = Array (startIndex + 1);
+            for(index = 0;index < startIndex; index++){
+                args[index] = arguments[index];
+            }
+            args[startIndex] = rest;
+            return func.apply(this, args);
+        };
+    };
+
+    var baseCreate = function (prototype) {
+        if(!_.isObject(prototype)) return {};
+        if(nativeCreate) return nativeCreate(prototype);
+        Ctor.prototype = prototype;
+        var result = new Ctor;
+        Ctor.prototype = null
+        return result;
+    };
+
+    var property = function (key) {
+        return function (obj) {
+            return oj == null ? void 0 : obj[key];
+        };
+    };
+
+    var MAX_ARRAY_INDEX = math.pow(2,53) - 1;
+    var getLength = property('length');
 
 
 
