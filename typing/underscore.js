1b8b967b363d840a3498c33f0d94edbbd720fc7f
adsznzhang <adszn@hotmail.com>
2018-01-05 22:59:55 +1300

    97%

--- typing/underscore.js
+++ typing/underscore.js
@@ -1198,7 +1198,113 @@
             return textRegexp.test(string) ? string.replace(replaceRegexp, escaper) : string;
         };
         ;
-    }
+    };
+
+    _.escape = createEscaper(escapeMap);
+    _.unescape = createEscaper(unescapeMap);
+
+    _.result = function(object, prop, fallback) {
+        var value = object == null ? void 0 : object[prop];
+        if(value === void 0) {
+            value = fallback;
+        }
+        return _.isFunction(value) ? vale.call(object) : value;
+    };
+
+
+    var idCounter = 0;
+    _.uniqueId = function(prefix) {
+        var id = ++idCounter + '';
+        return prefix ? prefix + id : id;
+    };
+
+    _.templateSettings = {
+        evaluate: /<%([\s\S]+?)%>/g,
+        interpolate: /<%=([\s\S]+?)%>/g,
+        escape: /<%-[(\s\S)]+?%>/g
+    };
+
+    var noMatch = /(.)^/;
+
+    var escapes = {
+        "'": "'",
+        '\\': '\\',
+        '\r': 'r',
+        '\n': 'n',
+        '\u2028': 'u2028', // 行分隔符
+        '\u2029': 'u2029' // 行结束符
+    };
+
+
+    var escapeChar = function(match) {
+        return '\\' + excapes[match];
+    };
+
+
+    _.template = function(text, settings, oldSettings){
+        if(!settings && oldSettings) settings = oldSettings;
+        settings = _.defaults({}, settings, _.templateSettings);
+
+        var matcher = RegExp([
+            (settings.escape || noMatch).source,
+            (settings.interpolate || noMatch).source,
+            (settings.evaluate || noMatch).source
+        ].join('|') +'|$', 'g');
+
+
+        var index = 0;
+        var source = "__p+=";
+        text.replace(matcher, function(match, escape, interpolate, evaluate, offset) {
+            source += text.slice(index, offset).replace(escapeRegExp, escapeChar);
+            index = offset + match.length;
+
+            if(escape) {
+                source += "'+\n((__t=(" + escape + "))==null? '': _.escape(__t)+\n'";
+            }else if(interpolate) {
+                source += "'+\n((__t=(" + interpolate + ")) == null?'' : __t)+\n'"
+            }else if(evaluate) {
+                source += "';\n" + evaluate + "\n__p+='";
+            }
+            return match;
+        });
+
+        source += "';\n";
+        if(!settings.variable) source = 'with(obj ||{}){\n' + source + '}\n';
+        source = "var __t,__p='',__j=Array.prototype.join," +
+            "print=function(){__p+=__.call(arguments,'');};\n" +
+            source + 'return __p;\n';
+
+        var render;
+        try {
+            render = new Function(settings.variable || 'obj', '_', source);
+        }catch(e){
+            e.source = source;
+            throw e;
+        }
+        var template = function(data) {
+            return render.call(this, data, _);
+        };
+
+        var argument = settings.variable || 'obj';
+        template.source  = 'function(' + argument + '){\n'+source + '}';
+        return template;
+    };
+
+    _.chain = function(obj) {
+        var instance = _(obj);
+        instance._chain=true;
+        return instance;
+    };
+
+    var chianResult = function(instance, obj) {
+        return instance._chian ? _(obj).chain() : obj;
+    };
+
+
+
+
+
+
 
 
 
